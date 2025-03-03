f7b1d2493b9beaf8359420a1bab2ab6b14b5faeb
ptsionis <tsionispanagiotis@gmail.com>
2025-03-03 19:28:59 +0200

    Added Google form URL for questions

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,8 @@
 </script>
 
 <template>
-  <footer class="w-full text-center text-xs text-muted-foreground self-end p-4">
-    Made by <a href="https://www.ptsionis.com/" class="hover:text-muted-foreground/90" target="_blank" rel="noopener noreferrer">Panagiotis Tsionis</a> &copy; {{ new Date().getFullYear() }}
+  <footer class="w-full flex flex-col justify-center items-center space-y-2 text-center text-muted-foreground text-xs self-end p-4">
+    <span>Have a new question in mind? Feel free to share it <a href="https://forms.gle/TR2KACsHtELi5FfXA" class="text-destructive hover:text-destructive/90" target="_blank" rel="noopener noreferrer">here</a> ❤️</span>
+    <span>Made by <a href="https://www.ptsionis.com/" class="hover:text-muted-foreground/90" target="_blank" rel="noopener noreferrer">Panagiotis Tsionis</a> &copy; {{ new Date().getFullYear() }}</span>
   </footer>
 </template>
