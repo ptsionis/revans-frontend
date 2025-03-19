488f0dbd9e6f988a4329a1b4a059efd89312a2c1
ptsionis <tsionispanagiotis@gmail.com>
2025-03-19 14:49:59 +0200

    Removed Facebook login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import Footer from '@/components/Footer.vue'
 import LoginButton from '@/components/LoginButton.vue'
-import Separator from '@/components/ui/separator/Separator.vue'
 import { useAuth } from '@/composables/useAuth'
 
-const { loginWithFacebook, loginWithGoogle } = useAuth()
+const { loginWithGoogle } = useAuth()
 </script>
 
 <template>
   <div class="min-h-dvh flex flex-col justify-center items-center">
     <main class="w-full flex flex-col flex-1 justify-center items-center">
       <div class="flex flex-col flex-1 justify-center items-stretch space-y-4">
-        <LoginButton text="Sign in with Facebook" icon="mdi:facebook" :callback="loginWithFacebook" />
-        <Separator label="or" />
+        <!-- <LoginButton text="Sign in with Facebook" icon="mdi:facebook" :callback="loginWithFacebook" />
+        <Separator label="or" /> -->
         <LoginButton text="Sign in with Google" icon="mdi:google" :callback="loginWithGoogle" />
       </div>
     </main>
