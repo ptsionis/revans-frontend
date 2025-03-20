da6d42107f48b46485f8fc8cda0671e6e7b05764
ptsionis <tsionispanagiotis@gmail.com>
2025-03-20 16:16:56 +0200

    Added logo and updated login and meta

---
+++ src/components/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="/images/logo/logo-128.webp" alt="Revans">
+</template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Footer from '@/components/Footer.vue'
 import LoginButton from '@/components/LoginButton.vue'
+import Logo from '@/components/Logo.vue'
+import Separator from '@/components/ui/separator/Separator.vue'
 import { useAuth } from '@/composables/useAuth'
 
 const { loginWithGoogle } = useAuth()
@@ -8,11 +10,21 @@ const { loginWithGoogle } = useAuth()
 
 <template>
   <div class="min-h-dvh flex flex-col justify-center items-center">
-    <main class="w-full flex flex-col flex-1 justify-center items-center">
-      <div class="flex flex-col flex-1 justify-center items-stretch space-y-4">
-        <!-- <LoginButton text="Sign in with Facebook" icon="mdi:facebook" :callback="loginWithFacebook" />
-        <Separator label="or" /> -->
-        <LoginButton text="Sign in with Google" icon="mdi:google" :callback="loginWithGoogle" />
+    <main class="w-full flex flex-col justify-center items-center flex-1 px-12 py-8">
+      <div class="flex flex-col justify-center items-center space-y-12">
+        <div class="flex flex-col justify-center items-center space-y-4">
+          <Logo />
+          <h3 class="text-center">
+            Play real-time PvP football quizzes against your friends!
+          </h3>
+          <span class="text-danger text-center text-sm font-light">Revans is currently in Alpha version and game data may be lost!</span>
+        </div>
+        <Separator />
+        <div class="flex flex-col flex-1 justify-center items-stretch space-y-4">
+          <!-- <LoginButton text="Sign in with Facebook" icon="mdi:facebook" :callback="loginWithFacebook" /> -->
+          <LoginButton text="Sign in with Google" icon="mdi:google" :callback="loginWithGoogle" />
+          <span class="text-xs font-thin text-muted-foreground">Login via Facebook is currently unavailable.</span>
+        </div>
       </div>
     </main>
     <Footer />
